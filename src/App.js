db1d4fbd912b21df95f3fa530806740c41e12f95
Jaye Popplewell <jlpopp01@gmail.com>
2020-11-04 14:36:45 -0500

    lets build it with react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+
+function App() {
+  return (
+    <div>
+    <Jumbotron>
+        <h1>Welcome to The Not So Google Form</h1>
+          <p>
+            This form is powered by React.
+          </p>
+    </Jumbotron>
+    </div>
+  );
+}
+
+export default App;
