834e41ad50a9f9a8a42014f1b5f1f5d92c12093e
Jaye Popplewell <jlpopp01@gmail.com>
2020-11-18 19:07:10 -0500

    There Will Be Beer

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
-import React from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-
-
-const url = 'https://api.punkapi.com/v2/beers/random'
-
-const fetchBeerData = async () => {
-  try {
-    const response = await fetch(`${url}`)
-    const data = await response.json()
-    console.log(data)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const displayData = data => {
-  document.getElementById('name').textContent = data.name
-  document.getElementById('tagline').textContent = data.tagline
-  document.getElementById('description').src = data.description
-  document.getElementById('abv').textContent = data.abv
-}
-
-fetchBeerData()
-
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div>
-    <Jumbotron className="Jumbo" align="center">
-        <h1>Welcome to That Time I Used Create React App</h1>
-          <p>
-            This will get you a random beer and present it's name, some info about the beer and the ever important ABV
-          </p>
-    </Jumbotron>
-    <main className="main">
-      <h1>Get A Random Beer</h1>
-      <h2 id="name"></h2>
-      <p id="tagline"></p>
-      <p id="description"></p>
-      <p id="abv"></p>
-    </main>
-    </div>
-  );
+
+  const [beer, randomBeer] = useState(null);
+
+  const beerAPI = "https://api.punkapi.com/v2/beers/random";
+
+  const fetchBeer = async () => {
+    const response = await axios.get(beerAPI)
+    randomBeer(response.data.[0])
+    };
+console.log(randomBeer);
+
+fetchBeer();
+
+    return (
+      <div className="App">
+        <h1 align="center">There Will Be Beer</h1>
+        <h2>This button will display a random beer from Punk API beer</h2>
+            <div className='text-center'>
+              <button className="fetch-button" onClick={fetchBeer}>
+                Fetch Beer
+              </button>
+            </div>
+      </div>
+    )
 }
 
 export default App;
