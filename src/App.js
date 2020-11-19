44b2e723711f4c7a3ab0f834dae0c9788587a354
Jaye Popplewell <jlpopp01@gmail.com>
2020-11-18 19:20:16 -0500

    logs beer to console, want button to display beer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
-function App() {
+class App extends React.Component {
 
-  const [beer, randomBeer] = useState(null);
+  state = {
+    randomBeer: []
+  }
 
-  const beerAPI = "https://api.punkapi.com/v2/beers/random";
+  componentDidMount() {
+    axios.get('https://api.punkapi.com/v2/beers/random')
+      .then(response => {
+        const randomBeer = response.data.[0];
+        console.log(randomBeer);
+      })
+  }
 
-  const fetchBeer = async () => {
-    const response = await axios.get(beerAPI)
-    randomBeer(response.data.[0])
-    };
-console.log(randomBeer);
-
-fetchBeer();
-
-    return (
-      <div className="App">
-        <h1 align="center">There Will Be Beer</h1>
-        <h2>This button will display a random beer from Punk API beer</h2>
-            <div className='text-center'>
-              <button className="fetch-button" onClick={fetchBeer}>
-                Fetch Beer
-              </button>
-            </div>
-      </div>
-    )
+    render() {
+      return (
+        <div className="App"
+          style={{
+            backgroundColor: 'blue',
+          }}>
+          <h1 align="center">There Will Be Beer</h1>
+          <h2>This button will display a random beer from Punk API beer</h2>
+              <div className='text-center'>
+                <button className="fetch-button">
+                  Fetch Beer
+                </button>
+              </div>
+        </div>
+      )
+    }
 }
 
 export default App;
