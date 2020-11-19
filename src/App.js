8bb86aa08b053d63e51dc932cdf731687935200d
Jaye Popplewell <jlpopp01@gmail.com>
2020-11-19 07:39:47 -0500

    Beer titles on Screen

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
+import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-class App extends React.Component {
+
+
+class App extends Component {
 
   state = {
-    randomBeer: []
+    beers: []
   }
 
   componentDidMount() {
     axios.get('https://api.punkapi.com/v2/beers/random')
-      .then(response => {
-        const randomBeer = response.data.[0];
-        console.log(randomBeer);
+      .then(res => {
+        console.log(res);
+        this.setState({ beers: res.data });
       })
   }
 
-    render() {
-      return (
-        <div className="App"
-          style={{
-            backgroundColor: 'blue',
-          }}>
-          <h1 align="center">There Will Be Beer</h1>
-          <h2>This button will display a random beer from Punk API beer</h2>
-              <div className='text-center'>
-                <button className="fetch-button">
-                  Fetch Beer
-                </button>
-              </div>
-        </div>
-      )
-    }
+
+
+  render() {
+    return (
+      <div className="Beer-App">
+        {this.state.beers.map(beer => <h1 key={beer.id}>{beer.name}</h1>)}
+      </div>
+    );
+  }
 }
+
 
 export default App;
